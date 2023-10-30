a02cf7dd9cde85c36481a18ec9a02727e0ab7107
Hariram1508 <hariaramhariaram@gmail.com>
2023-10-30 16:19:12 +0530

    today code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+ <Row >
+       <Col lg={5} md={1} sm={2} className='bg-primary p-5 col-lg-5'>Column1</Col>
+       <Col lg={5} md={2} sm={2} className='bg-success p-5 '>Column2</Col>
+       <Col lg={1} md={4} sm={2} className='bg-secondary p-5 '>Column3</Col>
+       <Col lg={1} md={5} sm={2} className='bg-danger p-5 '>Column4</Col>
+    </Row>
+  </Container>
   );
 }
 
