77d6be22e95082d5241f69a9727932f2b840db6f
Hariram1508 <hariaramhariaram@gmail.com>
2023-11-08 16:59:24 +0530

    about.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import { useState } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
-  const [name,setName]=useState('');
-  const [location,setLocation]=useState('');
-  const [fatherName,setfatherName]=useState('');
-  const [Detail,setDetail]=useState([]);
-  
-
-   const onsubmit =(e) =>{
-    e.preventDefault();
-    const newDetail ={
-      name,location,fatherName 
-   } 
-    setDetail([...Detail, newDetail]) 
-    setName("");
-    setLocation("");
-    setfatherName("");
-  }
-  return (
-    <div className=''>
-   <form className=''>
-    <input type="text"   onChange={(e) => setName(e.target.value)}placeholder='Enter your name '></input>
-    <input type="text"   onChange={(e) => setLocation(e.target.value)}placeholder='Enter your name '></input>
-    <input type="text"   onChange={(e) => setfatherName(e.target.value)}placeholder='Enter your name '></input>
-    <input type="submit" onClick={onsubmit}  value={'submit'} />
-   </form>
-     {Detail.map((info, index) =>(
-      <div key={index}>
-        <h1>Detail</h1> 
-         <p>Name: {info.name}</p>
-         <p>Location: {info.location}</p>
-         <p>father Name: {info.name}</p>
-      </div> 
-     ))}    
-   </div>
-
-  );
+  return(
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </Router>
+    </div>
+  )
 }
-
 export default App;
